9c58a7d2712f5d1a8677d0cb2aabb2daa2610dc2
Aristotle Pagaltzis <pagaltzis@gmx.de>
2021-10-25 04:06:36 +0200

    release 0.103

--- lib/Config/INI/Tiny.pm
+++ lib/Config/INI/Tiny.pm
@@ -3,7 +3,7 @@ use Carp ();
 
 package Config::INI::Tiny;
 
-our $VERSION = '0.102';
+our $VERSION = '0.103';
 
 sub new { my $class = shift; bless { section0 => '', line0 => 0, pairs => 0, @_ }, $class }
 
